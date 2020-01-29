08869c73c73d6df447d4c816f1ec4e1c133e4fa2
Kaldric <cjsyren@gmail.com>
2020-01-29 12:55:49 +0200

    Yhden pysäkin appi toimii

--- busspysakki.py
+++ busspysakki.py
@@ -20,16 +20,29 @@ currentTime = parsed['body'][0]['recordedAtTime']
 dictio = {}
 dictio = parsed['body']
 
+found = []
 
-
+currentTime = currentTime[11:16]
+print("Kello on: ", currentTime)
+minTime = int(currentTime[0:2]) * 60 + int(currentTime[3:6])
 for data in dictio:
     onwardCalls = data['monitoredVehicleJourney']['onwardCalls']
     for call in onwardCalls:
         if call['stopPointRef'] == stopPoint:
             busNumber = data['monitoredVehicleJourney']['lineRef']
             expectedArrival = call['expectedArrivalTime']
-            print("Bussinumero ", busNumber, " on pysäkillä ", expectedArrival, ".\n")
+            expectedArrival = expectedArrival[11:16]
+            minArrival = int(expectedArrival[0:2]) * 60 + int(expectedArrival[3:5])
+            minErotus = (minArrival - minTime)
+            print(minArrival, minTime)
+            foundOne = str("{:02}".format(minErotus)) + " minuutin päästä bussi (" + str(busNumber) + ") on pysäkillä kello: " + str(expectedArrival)
+            foundOne = str(foundOne)
+            found.append(foundOne)
 
+found.sort()
+
+for e in found:
+    print(e)
 
  
       

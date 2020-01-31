bf8dd5a14bb907ec021fec504b85a8f0178d1e00
Kaldric <cjsyren@gmail.com>
2020-01-31 10:05:17 +0200

    Funktioperusteinen aloitettu

--- BusStop.py
+++ BusStop.py
@@ -1,17 +1,16 @@
 import requests
+import json
 
-response = requests.get('http://data.itsfactory.fi/journeys/api/1/vehicle-activity')
 
-stopPoint = "http://data.itsfactory.fi/journeys/api/1/stop-points/5116"
+def getDatatoDictionary(url):  
+    return requests.get(url).json()
 
-json_data = response.json() if response and response.status_code == 200 else None
 
-found = []
+#busDictio = getDatatoDictionary("http://data.itsfactory.fi/journeys/api/1/vehicle-activity")
 
-for f in json_data['monitoredVehicleJourney']['onwardCalls']:
-    found.append(f)        
+#stopDictio = getDatatoDictionary("http://data.itsfactory.fi/journeys/api/1/stop-points")
 
-print(f)
-           # for call in json_data['monitoredVehicleJourney']['onwardCalls']:
-           # host_name = hoststatus.get('name')
-           # status_text = hoststatus.get('status_text')
+busDictio = getDatatoDictionary("http://data.itsfactory.fi/journeys/api/1/vehicle-activity")
+
+print(busDictio)
+

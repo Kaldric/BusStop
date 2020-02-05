7f1abac73421c0ea99383491ea937feba91be0d1
Kaldric <cjsyren@gmail.com>
2020-02-05 16:00:10 +0200

    Lisätty try except data streamin katkeamisen varalta.

--- Lambdasekoilua.py
+++ Lambdasekoilua.py
@@ -61,42 +61,46 @@ def busesForStop(foundStop, parsedBus, currentTime):                            
     arrivalInMin = ""
     minDifference = ""
     timeInMin = int(currentTime[0:2]) * 60 + int(currentTime[3:5])
-    for bus in parsedBus['body']:
-        onwardCalls = bus['monitoredVehicleJourney']['onwardCalls']
-        stopsAtKtori = False                                                                                                        #indeksit: 0-14
-        orderKtori = 0
-        stopsAtTAYS = False                                                                                                         #indeksit: 15-19
-        orderTAYS = 0
-        stopsAtBusStation = False                                                                                                   #indeksit: 20-21
-        orderBusStation = 0
-        for call in onwardCalls:
-            if call['stopPointRef'][-4:] in listOfStops:
-                    if call['stopPointRef'][-4:] in listOfStops[0:15]:
-                        stopsAtKtori = True
-                        orderKtori = call['order']
-                    if call['stopPointRef'][-4:] in listOfStops[15:20]:
-                        stopsAtTAYS = True
-                        orderTAYS = call['order']
-                    if call['stopPointRef'][-4:] in listOfStops[20:22]:
-                        stopsAtBusStation = True
-                        orderBusStation = call['order']
-        for call in onwardCalls:
-            if call['stopPointRef'][-4:] == foundStop[0][-4:]:
-                busNumber = bus['monitoredVehicleJourney']['lineRef']
-                expectedArrival = call['expectedArrivalTime']
-                callOrder = int(call['order'])
-                if callOrder > int(orderKtori):                                                                                     #verrataan pysäkkijärjestystä eli jottei keskustori näy truena, jos se on matkalla ennen katsottua pysäkkiä
-                    stopsAtKtori = False
-                if callOrder > int(orderTAYS):
-                    stopsAtKtori = False
-                if callOrder > int(orderBusStation):
-                    stopsAtKtori = False
-                expectedArrival = expectedArrival[11:16] 
-                arrivalInMin = int(expectedArrival[0:2]) * 60 + int(expectedArrival[3:5])
-                minDifference = arrivalInMin - timeInMin
-                foundOne = "<p>At {:5} bus number {:2} will be at the stop {} ({}) in {:3} minutes. Stops at: Town square {}, TAYS (hospital) {}, Bus station {}.<br></p>".format(str(expectedArrival), str(busNumber), foundStop[1], foundStop[2], str(minDifference), stopsAtKtori, stopsAtTAYS, stopsAtBusStation)
-                foundBuses.append(foundOne)
-           
+    try:
+        for bus in parsedBus['body']:
+            onwardCalls = bus['monitoredVehicleJourney']['onwardCalls']
+            stopsAtKtori = False                                                                                                        #indeksit: 0-14
+            orderKtori = 0
+            stopsAtTAYS = False                                                                                                         #indeksit: 15-19
+            orderTAYS = 0
+            stopsAtBusStation = False                                                                                                   #indeksit: 20-21
+            orderBusStation = 0
+            for call in onwardCalls:
+                if call['stopPointRef'][-4:] in listOfStops:
+                        if call['stopPointRef'][-4:] in listOfStops[0:15]:
+                            stopsAtKtori = True
+                            orderKtori = call['order']
+                        if call['stopPointRef'][-4:] in listOfStops[15:20]:
+                            stopsAtTAYS = True
+                            orderTAYS = call['order']
+                        if call['stopPointRef'][-4:] in listOfStops[20:22]:
+                            stopsAtBusStation = True
+                            orderBusStation = call['order']
+            for call in onwardCalls:
+                if call['stopPointRef'][-4:] == foundStop[0][-4:]:
+                    busNumber = bus['monitoredVehicleJourney']['lineRef']
+                    expectedArrival = call['expectedArrivalTime']
+                    callOrder = int(call['order'])
+                    if callOrder > int(orderKtori):                                                                                     #verrataan pysäkkijärjestystä eli jottei keskustori näy truena, jos se on matkalla ennen katsottua pysäkkiä
+                        stopsAtKtori = False
+                    if callOrder > int(orderTAYS):
+                        stopsAtKtori = False
+                    if callOrder > int(orderBusStation):
+                        stopsAtKtori = False
+   
+                    expectedArrival = expectedArrival[11:16] 
+                    arrivalInMin = int(expectedArrival[0:2]) * 60 + int(expectedArrival[3:5])
+                    minDifference = arrivalInMin - timeInMin
+                    foundOne = "<p>At {:5} bus number {:2} will be at the stop {} ({}) in {:3} minutes. Stops at: Town square {}, TAYS (hospital) {}, Bus station {}.<br></p>".format(str(expectedArrival), str(busNumber), foundStop[1], foundStop[2], str(minDifference), stopsAtKtori, stopsAtTAYS, stopsAtBusStation)
+                    foundBuses.append(foundOne)
+    except:
+        print('Live data temporarily unavailable for the search, please try again.')
+
     if len(foundBuses) > 0:
         if foundStop[0][-4:] == ("0522"):
             foundBuses.append("Note that this stop is on the bus station's side of the road. For the one on the other side look up bus stop number 0523.")
